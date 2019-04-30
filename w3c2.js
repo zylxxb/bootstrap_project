d2883576ac34c93c0b7a78aaf9724a2b3c8aceab
wmhello <871228582@qq.com>
2019-04-30 13:24:29 +0800

    CSS结构设计

---
+++ w3c2.js
@@ -0,0 +1,4 @@
+var height = parseInt(window.innerHeight);
+var contentHeight = height - 52
+document.querySelector('#container').style.height = contentHeight + 'px'
+document.querySelector('#sidebar').style.height = contentHeight + 'px'
